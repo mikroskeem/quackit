45b2cf174b8f306578489034f22804b5000e60f9
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-06-23 02:04:08 +0300

    Fix using 'main' package instead of 'quackit'
    Fixes: import "github.com/mikroskeem/quackit" is a program, not an importable package

--- parser_test.go
+++ parser_test.go
@@ -1,4 +1,4 @@
-package main
+package quackit
 
 import (
 	"testing"
--- stub.go
+++
@@ -1,4 +0,0 @@
-package main
-
-// This is just a temporary (or permanent?) fix for Travis Golang setup to shut up
-func main() {}
